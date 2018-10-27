d4b52df63cf44c3d7408d126196dbe1d7cb21cb7
emilia98 <emilia_nedialkova@abv.bg>
2018-10-27 23:26:14 +0300

    completed lab

---
+++ 03. Conditional Statements/Lab/05.PasswordGuess/PasswordGuess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05.PasswordGuess
+{
+    class PasswordGuess
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string password = "s3cr3t!P@ssw0rd";
+
+            if(input == password)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
