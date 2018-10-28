02c40ddaf3e3ed1941842028dcbb17476ea36524
emilia98 <emilia_nedialkova@abv.bg>
2018-10-28 23:53:34 +0200

    added the last of graded

---
+++ 03. Conditional Statements/Exercise/09.WorldSwimmingRecord/WorldSwimmingRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _09.WorldSwimmingRecord
+{
+    class WorldSwimmingRecord
+    {
+        static void Main()
+        {
+            double record = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timePerMeter = double.Parse(Console.ReadLine());
+
+            double time = distance * timePerMeter;
+            double delay = (int)(distance / 15) * 12.5;
+            double totalTime = time + delay;
+
+            if(totalTime < record)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {totalTime - record:f2} seconds slower.");
+            }
+        }
+    }
+}
