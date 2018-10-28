d0478c6a06d11757bbbc9de6a2e7a6babf18a920
emilia98 <emilia_nedialkova@abv.bg>
2018-10-28 23:16:36 +0200

    added three more

---
+++ 03. Conditional Statements/Exercise/05.TimePlus15Minutes/TimePlus15Minutes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05.TimePlus15Minutes
+{
+    class TimePlus15Minutes
+    {
+        static void Main()
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int newMinutes = minutes + 15;
+            int newHours = hours;
+
+            if(newMinutes >= 60)
+            {
+                newHours++;
+                newMinutes = newMinutes - 60;
+            }
+
+            if(newHours > 23)
+            {
+                // newHours = 24 - newHours;
+                newHours = 0;
+            }
+            
+            if(newMinutes < 10)
+            {
+                Console.WriteLine($"{newHours}:0{newMinutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{newHours}:{newMinutes}");
+            }
+        }
+    }
+}
---
+++ 03. Conditional Statements/Exercise/06.ThreeBrothers/ThreeBrothers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06.ThreeBrothers
+{
+    class ThreeBrothers
+    {
+        static void Main()
+        {
+            double timeOfA = double.Parse(Console.ReadLine());
+            double timeOfB = double.Parse(Console.ReadLine());
+            double timeOfC = double.Parse(Console.ReadLine());
+            double timeForFishing = double.Parse(Console.ReadLine());
+
+            double productivity_A = 1 / timeOfA;
+            double productivity_B = 1 / timeOfB;
+            double productivity_C = 1 / timeOfC;
+
+            double totalProductivity = productivity_A + productivity_B + productivity_C;
+            double timeForWork = 1 / totalProductivity;
+            double timeForBreak = timeForWork * 0.15;
+
+            double totalTime = timeForWork + timeForBreak;
+
+            // Console.WriteLine($"Cleaning time: {Math.Round(totalTime, 2)}");
+            Console.WriteLine($"Cleaning time: {totalTime:f2}");
+
+            if (totalTime < timeForFishing)
+            {
+                // резултатът трябва да е закръглен до следващото по-малко цяло число
+                double timeLeft = Math.Floor(timeForFishing - totalTime);
+                Console.WriteLine($"Yes, there is a surprise - time left -> {timeLeft} hours.");
+            }
+            else
+            {
+                // резултатът трябва да е закръглен до следващото по-голямо цяло число
+                double timeNeeded = Math.Ceiling(totalTime - timeForFishing);
+                Console.WriteLine($"No, there isn't a surprise - shortage of time -> {timeNeeded} hours.");
+            }
+        }
+    }
+}
