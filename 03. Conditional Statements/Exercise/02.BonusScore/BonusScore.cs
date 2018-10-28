6e81666ac0398fc47675bcbae8b4edf39490b2c4
emilia98 <emilia_nedialkova@abv.bg>
2018-10-28 22:01:28 +0200

    added some solved exercise problems

---
+++ 03. Conditional Statements/Exercise/02.BonusScore/BonusScore.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02.BonusScore
+{
+    class BonusScore
+    {
+        static void Main()
+        {
+            int score = int.Parse(Console.ReadLine());
+            double bonusScore = 0;
+
+            if(score <= 100)
+            {
+                bonusScore = 5;
+            }
+            else if(score > 100 && score <= 1000)
+            {
+                bonusScore = (0.2 * score);
+            }
+            else if(score > 1000)
+            {
+                bonusScore = (0.1 * score);
+            }
+            
+            // Проверка дали числото е четно
+            if(score % 2 == 0)
+            {
+                bonusScore += 1;
+            }
+
+            // Последна цифра се взема с (числото % 10)
+            else if(score % 10 == 5)
+            {
+                bonusScore += 2;
+            }
+
+            /* NOTE: Числото НЕ може да е едновременно четно и да завършва на 5. */
+            double finalResult = score + bonusScore;
+            Console.WriteLine(bonusScore);
+            Console.WriteLine(finalResult);
+        }
+    }
+}
---
+++ 03. Conditional Statements/Exercise/04.MetricsConverter/MetricsConverter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace _04.MetricsConverter
+{
+    class MetricsConverter
+    {
+        static void Main()
+        {
+            const double millimeters = 1000;
+            const double centimeters = 100;
+            const double miles = 0.000621371192;
+            const double inches = 39.3700787;
+            const double kilometers = 0.001;
+            const double feet = 3.2808399;
+            const double yards = 1.0936133;
+
+            double number = double.Parse(Console.ReadLine());
+            string metric_1 = Console.ReadLine();
+            string metric_2 = Console.ReadLine();
+
+            double result = number;
+
+            if(metric_1 == "m")
+            {
+                // като превърнем метър към метър, получаваме самото число
+                result = number;
+            }
+            else if(metric_1 == "mm")
+            {
+                result = number / millimeters;
+            }
+            else if (metric_1 == "cm")
+            {
+                result = number / centimeters;
+            }
+            else if (metric_1 == "mi")
+            {
+                result = number / miles;
+            }
+            else if (metric_1 == "in")
+            {
+                result = number / inches;
+            }
+            else if (metric_1 == "km")
+            {
+                result = number / kilometers;
+            }
+            else if (metric_1 == "ft")
+            {
+                result = number / feet;
+            }
+            else if (metric_1 == "yd")
+            {
+                result = number / yards;
+            }
+
+            if (metric_2 == "m")
+            {
+                // като превърнем метър към метър, получаваме самото число
+                result = result;
+            }
+            else if (metric_2 == "mm")
+            {
+                result = result * millimeters;
+            }
+            else if (metric_2 == "cm")
+            {
+                result = result * centimeters;
+            }
+            else if (metric_2 == "mi")
+            {
+                result = result * miles;
+            }
+            else if (metric_2 == "in")
+            {
+                result = result * inches;
+            }
+            else if (metric_2 == "km")
+            {
+                result = result * kilometers;
+            }
+            else if (metric_2 == "ft")
+            {
+                result = result * feet;
+            }
+            else if (metric_2 == "yd")
+            {
+                result = result * yards;
+            }
+
+            Console.WriteLine($"{result:f8}");
+        }
+    }
+}
