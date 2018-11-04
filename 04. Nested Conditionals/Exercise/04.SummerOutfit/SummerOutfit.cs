15a618760bb0c5a0161b7d745b06d80e669f984f
emilia98 <emilia_nedialkova@abv.bg>
2018-11-04 21:39:34 +0200

    added the exe

---
+++ 04. Nested Conditionals/Exercise/04.SummerOutfit/SummerOutfit.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace _04.SummerOutfit
+{
+    class SummerOutfit
+    {
+        static void Main()
+        {
+            int temperature = int.Parse(Console.ReadLine());
+            string partOfDay = Console.ReadLine();
+
+            string outfit = "";
+            string shoes = "";
+            
+            if(temperature >= 10 && temperature <= 18)
+            {
+                if(partOfDay == "Morning")
+                {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                // Можем да комбинираме по този начин, за да избегнем повтарянето на код
+                else if (partOfDay == "Afternoon" || partOfDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                /*
+                else if (partOfDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    outfit = "Moccasins";
+                }
+                */
+            }
+            else if (temperature > 18 && temperature <= 24)
+            {
+                if (partOfDay == "Morning")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (partOfDay == "Afternoon")
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                if (partOfDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            else if (temperature > 24)
+            {
+                if (partOfDay == "Morning")
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (partOfDay == "Afternoon")
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+                if (partOfDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            Console.WriteLine($"It's {temperature} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
---
+++ 04. Nested Conditionals/Exercise/08.Journey/Journey.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _08.Journey
+{
+    class Journey
+    {
+        static void Main()
+        {
+            decimal budget = decimal.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string destination = "";
+            string stayType = "";
+            decimal moneySpend = 0;
+            
+            if(budget <= 100)
+            {
+                destination = "Bulgaria";
+
+                if(season == "summer")
+                {
+                    stayType = "Camp";
+                    moneySpend = 0.30m * budget;
+                }
+                else if(season == "winter")
+                {
+                    stayType = "Hotel";
+                    moneySpend = 0.70m * budget;
+                }
+            }
+            else if(budget > 100 && budget <= 1000)
+            {
+                destination = "Balkans";
+
+                if (season == "summer")
+                {
+                    stayType = "Camp";
+                    moneySpend = 0.40m * budget;
+                }
+                else if (season == "winter")
+                {
+                    stayType = "Hotel";
+                    moneySpend = 0.80m * budget;
+                }
+            }
+            else
+            {
+                destination = "Europe";
+                moneySpend = 0.90m * budget;
+                stayType = "Hotel";
+            }
+
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{stayType} - {moneySpend:f2}");
+        }
+    }
+}
