d45b3ff28c893a78f950e6d82b409afd71d2376e
emilia98 <emilia_nedialkova@abv.bg>
2018-11-10 09:07:53 +0200

    added part of lab

---
+++ 05. While Loop/Lab/01.NumberInRange/NumberInRange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01.NumberInRange
+{
+    class NumberInRange
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            
+            while (number < 1 || number > 100)
+            {
+                Console.WriteLine("Invalid number!");
+                number = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"The number is: {number}");
+        }
+    }
+}
---
+++ 05. While Loop/Lab/05.Walking/Walking.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _05.Walking
+{
+    class Program
+    {
+        static void Main()
+        {
+            int totalSteps = 0;
+            bool hasGoneHome = false;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if(input == "Going home")
+                {
+                    // int stepsToHome = int.Parse(Console.ReadLine());
+                    // totalSteps += stepsToHome;
+                    hasGoneHome = true;
+                    continue;
+                    
+
+                   //  break;
+                    // continue;
+                    // break;
+                }
+
+                int steps = int.Parse(input);
+                totalSteps += steps;
+
+                if (totalSteps >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    break;
+                }
+
+                if(hasGoneHome)
+                {
+                    int stepsLeftToDo = 10000 - totalSteps;
+                    Console.WriteLine($"{stepsLeftToDo} more steps to reach goal.");
+                    break;
+                }
+
+            }
+        }
+    }
+}
