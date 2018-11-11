105562c0c3de5fff66bd7b90b72a6775b7388020
emilia98 <emilia_nedialkova@abv.bg>
2018-11-11 23:24:41 +0200

    added exe

---
+++ 05. While Loop/Exercise/05.Coins/Coins.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _05.Coins
+{
+    class Coins
+    {
+        static void Main()
+        {
+            double st = double.Parse(Console.ReadLine());
+            int totalCoins = 0;
+
+            int money = (int)(st * 100);
+            while (money > 0)
+            {
+                // Колко пъти се съдържат 200 стотинки (2 лева) в сумата
+                if (money / 200 > 0)
+                {
+                    money -= 200;
+                    totalCoins++;
+                }
+                // Колко пъти се съдържат 100 стотинки (1 лев) в сумата
+                else if (money / 100 > 0)
+                {
+                    money -= 100;
+                    totalCoins++;
+                }
+                // Колко пъти се съдържат 50 стотинки в сумата
+                else if (money / 50 > 0)
+                {
+                    money -= 50;
+                    totalCoins++;
+                }
+                else if (money / 20 > 0)
+                {
+                    money -= 20;
+                    totalCoins++;
+                }
+                else if (money / 10 > 0)
+                {
+                    money -= 10;
+                    totalCoins++;
+                }
+                else if (money / 5 > 0)
+                {
+                    money -= 5;
+                    totalCoins++;
+                }
+                else if (money / 2 > 0)
+                {
+                    money -= 2;
+                    totalCoins++;
+                }
+                else if (money / 1 > 0)
+                {
+                    money -= 1;
+                    totalCoins++;
+                }
+            }
+
+            Console.WriteLine(totalCoins);
+        }
+    }
+}
---
+++ 05. While Loop/Exercise/06.Cake/Cake.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    class Cake
+    {
+        static void Main()
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int totalPieces = width * length;
+            int piecesLeft = totalPieces;
+            int piecesNeeded = 0;
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+                
+                // Ако сме въвели STOP, излизаме от цикъла
+                if (input == "STOP")
+                {
+                    break;
+                }
+
+                // Ако не сме въвели STOP, можем да парснем входа към число
+                int piecesTaken = int.Parse(input);
+                
+                if (piecesTaken > piecesLeft)
+                {
+                    piecesNeeded = piecesTaken - piecesLeft;
+                    piecesLeft = 0;
+                    break;
+                }
+
+                piecesLeft -= piecesTaken;
+            }
+
+            if(piecesLeft > 0)
+            {
+                Console.WriteLine($"{piecesLeft} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {piecesNeeded} pieces more.");
+            }
+        }
+    }
+}
