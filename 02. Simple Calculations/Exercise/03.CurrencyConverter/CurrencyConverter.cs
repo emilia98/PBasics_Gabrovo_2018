7b66fb628fea4777f381b617d25cdccf9e9c8ab4
emilia98 <emilia_nedialkova@abv.bg>
2018-11-23 00:29:01 +0200

    added more solutions (COMPLETED)

---
+++ 02. Simple Calculations/Exercise/03.CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _03.CurrencyConverter
+{
+    class CurrencyConverter
+    {
+        static void Main()
+        {
+            decimal toConvert = decimal.Parse(Console.ReadLine());
+            string inputCurrency = Console.ReadLine();
+            string outputCurrency = Console.ReadLine();
+
+            decimal result = 0m;
+            decimal toBGN = 0m;
+
+            if (inputCurrency == "BGN")
+            {
+                toBGN = toConvert;
+            }
+            else if (inputCurrency == "USD")
+            {
+                toBGN = toConvert * 1.79549m;
+            }
+            else if (inputCurrency == "EUR")
+            {
+                toBGN = toConvert * 1.95583m;
+            }
+            else if (inputCurrency == "GBP")
+            {
+                toBGN = toConvert * 2.53405m;
+            }
+
+            if (outputCurrency == "BGN")
+            {
+                result = toBGN;
+            }
+            else if (outputCurrency == "USD")
+            {
+                result = toBGN / 1.79549m;
+            }
+            else if (outputCurrency == "EUR")
+            {
+                 result = toBGN / 1.95583m;
+            }
+            else if(outputCurrency == "GBP")
+            {
+                 result = toBGN / 2.53405m;
+            }
+
+            Console.WriteLine($"{Math.Round(result, 2)} {outputCurrency}");
+        }
+    }
+}
---
+++ 02. Simple Calculations/Exercise/06.CharityCampaign/CharityCampaign.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _06.CharityCampaign
+{
+    class CharityCampaign
+    {
+        static void Main()
+        {
+            int days = int.Parse(Console.ReadLine());
+            int bakersCount = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int wafers = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            // Намираме какво количество торти, гофрети и палачинки е правено за ден
+            int cakesPerDay = cakes * bakersCount;
+            int wafersPerDay = wafers * bakersCount;
+            int pancakesPerDay = pancakes * bakersCount;
+
+            // Намираме по колко пари е правил всеки тип сладкарско изделие за един ден
+            decimal moneyPerDay_cakes = cakesPerDay * 45;
+            decimal moneyPerDay_wafers = wafersPerDay * 5.80m;
+            decimal moneyPerDay_pancakes = pancakesPerDay * 3.20m;
+
+            decimal totalMoneyFromCakes = moneyPerDay_cakes * days;
+            decimal totalMoneyFromWafers = moneyPerDay_wafers * days;
+            decimal totalMoneyFromPancakes = moneyPerDay_pancakes * days;
+
+            decimal totalMoney = totalMoneyFromCakes + totalMoneyFromWafers + totalMoneyFromPancakes;
+            // Не трябва да умножаваме така: (1 / 8), защото този израз връща като резултат 0
+            decimal expenses = totalMoney * (1.0m / 8);
+            
+            // Намираме чистата печалба, като от всички спечелени пари изваждаме разходите
+            decimal moneyEarned = totalMoney - expenses;
+            Console.WriteLine($"{moneyEarned:f2}");
+        }
+    }
+}
