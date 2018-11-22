e4b2758099c799254417a468a95b11ca13d5fd04
emilia98 <emilia_nedialkova@abv.bg>
2018-11-22 22:19:31 +0200

    added few more solved (71%)

---
+++ 02. Simple Calculations/Exercise/01.2DRectangleArea/RectangleArea.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._2DRectangleArea
+{
+    class RectangleArea
+    {
+        static void Main()
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            // Дължината на правоъгълника се намира като вземем 
+            // x на едната точка и x на другата точка
+            // и ги извадим. Тъй като не знаем коя точка се намира
+            // по-надясно, за да намерим дължината, вземаме намерената разлика по модул
+            double length = Math.Abs(x1 - x2);
+            // Аналогично и за ширината
+            double width = Math.Abs(y1 - y2);
+            
+            // Намираме лицето
+            double area = length * width;
+            // Намираме периметъра
+            double perimeter = 2 * length + 2 * width; // 2 * (length + width);
+
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+        }
+    }
+}
---
+++ 02. Simple Calculations/Exercise/04.TailoringWorkshop/TailoringWorkshop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04.TailoringWorkshop
+{
+    class TailoringWorkshop
+    {
+        static void Main()
+        {
+            int tablesCount = int.Parse(Console.ReadLine());
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+
+            // Общата площ на покривките
+            double tableClothsArea = tablesCount * (length + 2 * 0.30) * (width + 2 * 0.30);
+            // Общата площ на каретата
+            double checkedClothsArea = tablesCount * (length / 2) * (length / 2);
+
+            // Намираме каква е общата цена на покривките в ДОЛАРИ (общата им площ * цената за един квадратен метър)
+            decimal totalPriceOfTableCloths = (decimal)tableClothsArea * 7.00m;
+            // Намираме каква е общата цена на каретата в ДОЛАРИ (общата им площ * цената за един квадратен метър)
+            decimal totalPriceOfCheckedCloths = (decimal)checkedClothsArea * 9.00m;
+
+            decimal totalPriceInDollars = totalPriceOfTableCloths + totalPriceOfCheckedCloths;
+            decimal totalPriceInLeva = totalPriceInDollars * 1.85m;
+
+            Console.WriteLine($"{totalPriceInDollars:f2} USD");
+            Console.WriteLine($"{totalPriceInLeva:f2} BGN");
+        }
+    }
+}
